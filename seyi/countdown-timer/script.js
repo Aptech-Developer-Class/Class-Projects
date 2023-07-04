566f76472c3a8ad41e021a16ab7e326146f6eee7
Japheth <japhethyakubu26@gmail.com>
2023-07-04 11:35:01 +0100

    SPACE WEBSITE

--- seyi/countdown-timer/script.js
+++ seyi/countdown-timer/script.js
@@ -1,8 +1,8 @@
-const countdownDate = new Date("Apr 1, 2023 00:00:00").getTime();
+const countdownDate = new Date("Apr 25, 2023 00:00:00").getTime();
 const y = setInterval(function(){
     const currentDate = new Date().getTime();
     const difference = countdownDate - currentDate;
-
+    
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
